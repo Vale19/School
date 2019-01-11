c918812dcd17d11695ce265f5bd73b4f2e1b6047
Vale19 <valentinoichim@gmail.com>
2019-01-11 11:31:39 +0100

    Update cc.cpp

--- Progetti Dev-C++4AI/cc.cpp
+++ Progetti Dev-C++4AI/cc.cpp
@@ -1,38 +1,55 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
-class CC
-
-{
+fstream f1;
+int i = 0;
+class CC{
 	string correntista;
 	string nc;
 	float saldo;
+	
+	struct op{
+		string data;
+		float importo;
+	};
+	op lista[1000];
+	
 	private:
 		string citta, indirizzo;
+		
 	public:
+		int nop;
 		CC (float a)
 		{
 			saldo = a;
+			nop = 0;
 		}
 		
-		void prelievo (float a) {
+		
+		void prelievo (float a, string data) {
 			if (saldo > a) {
 				saldo -= a;
 				cout << "Il saldo attuale e' di euro "<< saldo << endl;
-			} else {
-				cout << "Non e' possibile effettuare il prelievo di una cifra\n";
+				lista[nop].importo = -a;
+				lista[nop].data = data;
+				nop++;
 			}
+			else
+				cout << "Non e' possibile effettuare il prelievo di questa cifra\n";
 		}
 		
-		void versamento (float a) {
+		void versamento (float a , string data) {
 			saldo += a;
 			cout << "Il saldo attuale e' di euro " << saldo << endl;
+			lista[nop].importo = a;
+			lista[nop].data = data;
+			nop++;
 		}
 		
-		void stampa
-		(){
+		void stampa() {
 			cout << "Numero conto " << nc << endl;
 			cout << "Correntista " << correntista << endl;
 			cout << "Saldo " << saldo <<endl;
@@ -40,24 +57,31 @@ class CC
 			cout << "Indirizzo " << indirizzo << endl;
 		}
 		
-		void crea()
-		{
+		void crea() {
 			cout << "Inserisci numero conto " << endl;
 			cin >> nc;
 			cout << "Inserisci il cognome del correntista " << endl;
 			cin>>correntista; 
 		}
 		
-		void cambioresidenza(){
+		void cambioresidenza() {
 			cout << "Inserisci la citta " << endl;
 			cin >> citta;
 			cout << "Inserisci l'indirizzo " << endl;
 			cin >> indirizzo;
 		}
+		void salvataggio() {
+			f1.open("C:\\Users\\N10_4A\\Desktop\\Programmic++\\file.txt", ios :: app);
+			f1 << lista[i + 1].importo << lista[i + 1].data;
+			for(i = 1; i < nop; i++){
+				f1 << lista[i].importo << lista[i].data;
+			}
+		}
 };
 
 int main(){
 	int scelta;
+	string data;
 	float a;
 	CC conto (0);
 	conto.crea();
@@ -67,19 +91,24 @@ int main(){
 			cout << "2.versamento\n";
 			cout << "3.stampa dati del conto\n";
 			cout << "4.Cambia residenza\n";
+			cout << "5.Salvataggio \n";
 			cout << "0.esci\n";
 			cout << "fai una scelta\n";
 			cin >> scelta;
 			switch (scelta){
 				case 1:
+					cout << "Inserire la data corrente " << endl;
+					cin >> data;
 					cout << "Inserire la cifra da prelevare ";
 					cin >> a;
-					conto.prelievo(a);
+					conto.prelievo(a, data);
 					break;
 				case 2:
+					cout << "Inserire la data corrente " << endl;
+					cin >> data;
 					cout << "Inserire la cifra da versare ";
 					cin >> a;
-					conto.versamento(a);
+					conto.versamento(a, data);
 					break;
 					
 				case 3:
@@ -88,12 +117,14 @@ int main(){
 				case 4:
 					conto.cambioresidenza();
 					break;
+				case 5:
+					conto.salvataggio();
 				case 0:
 					scelta = 0;
 					break;
 			}
 			
 		}
-		while (scelta!=0);
+		while (scelta !=0 );
 		//return(0);
 }
