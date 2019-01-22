3018cfef57f3435daeaa1faf74b42d5dc1e233ce
Vale19 <valentinoichim@gmail.com>
2019-01-22 22:12:04 +0100

    Update ng.cpp

--- Progetti Dev-C++4AI/ng.cpp
+++ Progetti Dev-C++4AI/ng.cpp
@@ -10,9 +10,9 @@ class giorno{
 	public:
 		void inserisci(int dt){
 			ng = dt;
-			cout << "Inserire la tempeeratura minima del giorno " << ng << endl;
+			cout << "Inserire la temperatura minima del giorno " << ng << endl;
 			cin >> tmin;
-			cout << "Inserire la temperatura massima del giorno"
+			cout << "Inserire la temperatura massima del giorno " << ng << endl;
 			cin >> tmax;
 		}
 		
@@ -42,5 +42,52 @@ const int NG = 31;
 int main(){
 	
 	int i, sc;
-	giorno dic[NG];
+	giorno dic[NG]; //Vettore che contiene 31 istanze della classe Giorno.
+	int min, p, m;
+	for(int i = 0; i < NG; i++)
+		dic[i].inserisci(i+1);
+	do{
+		cout << "\n\n\n1.stampa dell'escursione termica giornaliera\n";
+		cout << "\n\n\n2.stampa del giorno piu' freddo\n";
+		cout << "\n\n\n3.stampa dei giorni in cui la temperatura e' rimasta sopra lo zero\n";
+		cout << "\n\n\n4.stampa della media delle temperature medie giornaliere\n";
+		cout << "\n\n\n0.esci\n";
+		cout << "\n\nFai una scelta\n";
+		cin >> sc;
+		cout << endl;
+		switch (sc){
+			case 1:
+				for(int i = 0; i < NG; i++)
+					cout << "L'escursione termica del giorno " << dic[i].getng() << " e': " << dic[i].differenza() << endl;
+				break;
+				
+			case 2:
+				min = dic[0].getng();
+				p = 0;
+				for(int i = 1; i < NG; i++)
+					if(dic[i].gettmin() < min){
+						min = dic[i].gettmin();
+						p = 1;
+					}
+				cout << "Il giorno piu' freddo e' stato il " << dic[p].getng() << " con gradi: " << min << endl;
+				break;
+				
+			case 3:
+				cout << "Giorni in cui la temperatura e' rimasta sopra lo zero:\n\n";
+				for(int i = 0; i < NG; i++)
+					dic[i].stampa(0);//Passa temperatura 0 al metodo
+				break;
+				
+			case 4:
+				m = 0;
+				for(int i = 0; i < NG; i++)
+					m = m + dic[i].mediag();
+				m = m / NG;
+				cout << "La temperatura media del mese e': " << m << endl;
+				break;
+				
+		}
+	}
+	while (sc != 0);
+	return(0);
 }
